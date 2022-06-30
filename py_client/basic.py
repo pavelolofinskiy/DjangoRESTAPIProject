b1a0cd632f4ff45f7facadc23ad90e31a7eae870
PavelOlofinskiy <pavelolofinskiy@gmail.com>
2022-06-30 12:21:00 +0300

    create first API view

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,8 +1,7 @@
 import requests
 
 
-endpoint = "http://localhost:8000/"
+endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, json={"query": "Hello world"})
-print(get_response.text)
-print(get_response.status_code)
+get_response = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello world"})
+print(get_response.json())
