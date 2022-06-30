fb0d93d3a5c4ff67246a2f2b6ee3a7aabe9fb3ac
PavelOlofinskiy <pavelolofinskiy@gmail.com>
2022-06-30 11:45:20 +0300

    run django project

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,10 +1,8 @@
 import requests
 
 
-endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/"
 
 get_response = requests.get(endpoint, json={"query": "Hello world"})
-print(get_response.text) # print raw text response
-
-print(get_response.json())
+print(get_response.text)
 print(get_response.status_code)
