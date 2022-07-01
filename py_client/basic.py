1377157c774d1378df27b956067a5fe123599741
PavelOlofinskiy <pavelolofinskiy@gmail.com>
2022-07-01 09:38:27 +0300

    make Django Model Instance to Dictionary

--- py_client/basic.py
+++ py_client/basic.py
@@ -3,5 +3,7 @@ import requests
 
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, params= {"product_id": 123})
+get_response = requests.get(endpoint, json={"product_id": 123})
+#print(get_response.headers)
+#print(get_response.text)
 print(get_response.json())
