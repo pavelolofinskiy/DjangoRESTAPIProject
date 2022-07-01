3916bfaaf69fa2e93ff5ff62312430d5efac9f46
PavelOlofinskiy <pavelolofinskiy@gmail.com>
2022-07-01 09:49:19 +0300

    make REST Framework View & Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,15 @@
-import json
 from django.forms.models import model_to_dict
-from django.http import JsonResponse, HttpResponse
 from products.models import Product
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
         data = model_to_dict(model_data, fields=['id', 'title', 'price'])
-    return JsonResponse(data)
-   #     json_data_str = json.dumps(data)
-   # return HttpResponse(json_data_str, headers={'content-type': 'application/json'})
+    return Response(data)
+
 
