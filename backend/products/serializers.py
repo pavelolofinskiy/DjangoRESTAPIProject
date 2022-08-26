ba97e8f776c474966cc9da63fb6535cc97c44687
PavelOlofinskiy <pavelolofinskiy@gmail.com>
2022-08-26 09:29:08 +0300

    work with Related Fields & Foreign Key Serializer

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,9 +1,23 @@
 from rest_framework import serializers
 from .models import Product
+from api.serializers import UserPublicSerializer
 from rest_framework.reverse import reverse
 from . import validators
 
+
+class ProductInlineSerializer(serializers.Serializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='product-detail',
+        lookup_field='pk',
+        read_only='True'
+    )
+    title = serializers.CharField(read_only=True)
+
+
 class ProductSerializer(serializers.ModelSerializer):
+    owner = UserPublicSerializer(source='user', read_only=True,)
+    related_products = ProductInlineSerializer(source='user.product_set.all', read_only=True, many=True)
+    my_user_data = serializers.SerializerMethodField(read_only=True)
     my_discount = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(
@@ -12,9 +26,12 @@ class ProductSerializer(serializers.ModelSerializer):
     )
     title = serializers.CharField(validators=[validators.validate_title_no_hello, validators.unique_product_title])
     # name = serializers.CharField(source='title', read_only=True)
+    # email = serializers.EmailField(source='user.email', read_only=True)
     class Meta:
         model = Product
         fields = [
+            'owner',
+            # 'email',
             'url',
             'edit_url',
             'pk',
@@ -22,8 +39,14 @@ class ProductSerializer(serializers.ModelSerializer):
             'content',
             'price',
             'sale_price',
-            'my_discount'
+            'my_discount',
+            'my_user_data',
+            'related_products',
         ]
+    def get_my_user_data(self, obj):
+        return {
+            'username': obj.user.username
+        }
     #def validate_title(self, value):
     #    request = self.context.get('request')
     #    user = request.user
