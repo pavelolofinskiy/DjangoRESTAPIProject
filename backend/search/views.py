2d9f4ae710aff040ccc7e41d8d140112f7ebccb6
PavelOlofinskiy <pavelolofinskiy@gmail.com>
2022-09-01 10:33:27 +0300

    safety commit

--- backend/search/views.py
+++ backend/search/views.py
@@ -1,9 +1,22 @@
 from rest_framework import generics
+from rest_framework.response import Response
 
 from products.models import Product
 from products.serializers import ProductSerializer
 
-class SearchListView(generics.ListAPIView):
+from . import client
+
+class SearchListView(generics.GenericAPIView):
+    def get(self, request, *args, **kwargs):
+        query = request.GET.get('q')
+        tag = request.GET.get('tag') or None
+        if not query:
+            return Response('', status=400)
+        results = client.perform_search(query, tags=[tag])
+        return Response(results)
+
+
+class SearchListOldView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
